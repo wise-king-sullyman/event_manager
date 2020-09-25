14b35274cef021e9e4e124dd2df09bfcda566f24
wise-king-sullyman <asullivan96@live.com>
2020-09-25 18:01:52 -0400

    Added Google civic API integration for legislator lookup

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,9 @@
 
 require "csv"
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -12,5 +16,17 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    legislators = legislators.officials
+
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+    
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
